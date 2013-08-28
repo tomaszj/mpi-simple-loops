62a90a35afaddbdbc4e11a98a8cbcb2db238d90c
Tomasz Janeczko <janecztom@gmail.com>
2013-08-28 23:40:15 +0200

    Added proper implementation of MPI_RunLoops using MPI_Scatter.

--- MPI_RunLoops/MPI_RunLoopsScatter.cpp
+++ MPI_RunLoops/MPI_RunLoopsScatter.cpp
@@ -4,24 +4,32 @@
 #include "stdafx.h"
 #include <cassert>
 #include <mpi.h>
-#include <time.h>
 
-#define LOOPS_COUNT 100
+#define DEFAULT_ITERATIONS_COUNT 1000
 #define TASK_TAG 1024
-#define DIE_TAG 666
-
 
 using namespace std;
 
 int process_loops(int loops);
-void main_process(int world_size);
-void worker_process(int rank, int world_size);
+void perform_process(int total_iterations, int current_rank, int world_size);
 
 int main(int argc, char* argv[])
 {
-	double tic = MPI_Wtime();
+	int total_iterations = 0;
 
-    MPI_Init(&argc, &argv);
+	if (argc == 2)
+	{
+		char *input = argv[1];
+		total_iterations = strtol(input, NULL, 10);
+	}
+	else
+	{
+		total_iterations = DEFAULT_ITERATIONS_COUNT;
+	}
+
+	MPI_Init(&argc, &argv);
+
+	double tic = MPI_Wtime();
 
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -29,17 +37,10 @@ int main(int argc, char* argv[])
     int current_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &current_rank);
 
-    if (current_rank == 0)
-    {
-        main_process(world_size);
-    }
-    else
-    {
-        worker_process(current_rank, world_size);
-    }
+	perform_process(total_iterations, current_rank, world_size);
 
-    MPI_Finalize();
 	double toc = MPI_Wtime();
+    MPI_Finalize();
 
     if (current_rank == 0)
     {
@@ -49,87 +50,66 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-void main_process(int world_size)
+void perform_process(int total_iterations, int current_rank, int world_size)
 {
-    // Coordinator process
-    cout << "[Manager] Starting coordinator process" << endl;
+    // Start of process
+	cout << "[Node #" << current_rank << "] Starting process" << endl;
 
-    int sent_work_count = 0;
-    int received_work_count = 0;
-    long long total_processed_loops = 0;
+	int *iterations_distribution = new int[world_size];
 
-    // Distribute initial work
-    for (int i = 1; i < world_size; i++)
-    {
-        MPI_Send(&sent_work_count, 1, MPI_INT, i, TASK_TAG, MPI_COMM_WORLD); 
-        cout << "[Manager] Sent work #" << sent_work_count << " to #" << i << endl;
-            
-        sent_work_count++;
+	if (current_rank == 0)
+	{
+		int iterations_left = total_iterations;
+		int iterations_per_worker = total_iterations / world_size;
+	
+		// Fill in a number of loops except for the last worker
+		for (int i = 0; i < world_size - 1; i++)
+		{
+			iterations_distribution[i] = iterations_per_worker;
+			iterations_left -= iterations_per_worker;
+		}
+
+		// Last worker gets the remaining loops to do
+		iterations_distribution[world_size - 1] = iterations_left;
+	
+		cout << "[Node #0] Base nodes iterations: " << iterations_per_worker << " last node: " << iterations_left << endl;
+	}
+	
+	// Distribute and perform the work
+	int iterations;
+	MPI_Scatter(iterations_distribution, 1, MPI_INT, &iterations, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	cout << "[Node #" << current_rank << "] Got " << iterations << " iterations to perform." << endl;
+
+	// Perform the work and collect the result
+	long long total_loops = 0;
+	for (int i = 0; i < iterations; i++)
+	{
+        int loops_to_perform = 10000000;
+        long result = process_loops(loops_to_perform);
+		total_loops += result;
     }
 
-    // Wait for incoming responses and deal remaining work
-    while (received_work_count < LOOPS_COUNT)
-    {
-        long processed_loops;
-        MPI_Status status;
-        MPI_Recv(&processed_loops, 1, MPI_LONG, MPI_ANY_SOURCE, TASK_TAG, MPI_COMM_WORLD, &status);
+	// Gather incoming responses
+	long long *received_loops = new long long[world_size];
+	MPI_Gather(&total_loops, 1, MPI_LONG_LONG, received_loops, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+    
+	if (current_rank == 0)
+	{
+		long long total_processed_loops = 0;
+		for (int i = 0; i < world_size; i++)
+		{
+			total_processed_loops += received_loops[i];
+		}
 
-        received_work_count++;
-        total_processed_loops += processed_loops;
+		cout << "[Node #0] Finished work with " << total_processed_loops << " processed loops" << endl;
+	}
 
-        cout << "[Manager] " << processed_loops << " loops from worker #" << status.MPI_SOURCE << endl;
+	cout << "[Node #" << current_rank << "] dies." << endl;
 
-        if (sent_work_count < LOOPS_COUNT)
-        {
-            MPI_Send(&sent_work_count, 1, MPI_INT, status.MPI_SOURCE, TASK_TAG, MPI_COMM_WORLD);
-            cout << "[Manager] Sent work #" << sent_work_count << " to #" << status.MPI_SOURCE << endl;
-
-            sent_work_count++;
-        }
-    }
-
-    // Kill the workers
-    for (int i = 1; i < world_size; i++)
-    {
-        int kill_message = -1;
-        MPI_Send(&kill_message, 1, MPI_INT, i, DIE_TAG, MPI_COMM_WORLD);
-    }
-
-    cout << "[Manager] Finished work with " << total_processed_loops << " processed loops" << endl;
-}
-
-void worker_process(int rank, int world_size)
-{
-    // Worker process.
-    cout << "[Worker #" << rank << "] Started." << endl;
-
-    while(1)
-    {
-        int received_data;
-        MPI_Status status;
-        MPI_Recv(&received_data, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-        if (status.MPI_TAG == TASK_TAG)
-        {
-            int current_loop_id = (int)received_data;
-            cout << "[Worker #" << rank << "] Received work #" << current_loop_id << endl;
-            int loops_to_perform = current_loop_id * 5000000;
-            long result = process_loops(loops_to_perform);
-
-            MPI_Send(&result, 1, MPI_LONG, 0, TASK_TAG, MPI_COMM_WORLD);
-            cout << "[Worker #" << rank << "] Sent " << result << " loops as work #" << current_loop_id << endl;
-        }
-        else if (status.MPI_TAG == DIE_TAG)
-        {
-            cout << "[Worker #" << rank << "] Killed." << endl;
-            break;
-        }
-        else
-        {
-            // Should have discovered a proper tag already.
-            MPI_Abort(MPI_COMM_WORLD, 666);
-        }
-    }
+	// Clean up
+	delete[] iterations_distribution;
+	delete[] received_loops;
 }
 
 int process_loops(int loops)
